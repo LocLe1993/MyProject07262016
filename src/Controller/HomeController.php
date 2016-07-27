fe56a2fdd654867d894c5643859da959edc4471e
Le Phu Loc <le.loc@wiredbeans.co.jp>
2016-07-27 17:39:20 +0700

    Change code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,12 @@ namespace MyProject\Controller;
 class HomeController {
 	public function index(\Silex\Application $app) {
 		$amount = $app['const']['username']['max'];
-		return $app['twig']->render('index.html.twig',array('str'=> '60,000.00','amount' => $amount));
+		return $app['twig']->render('index.html.twig',
+				array(
+						'str'=> '60,000.00',
+						'amount' => $amount,
+						'header_title' => 'Index - Home',
+						'title' => 'home'
+				));
 	}
 }
